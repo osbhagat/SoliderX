e7567c8219936ba060655e7c7734ddc95009ac4d
Omkar Bhagat <osbhagat90@gmail.com>
2024-02-26 17:12:14 +0530

    Increasing delay

--- LED_Blink.py
+++ LED_Blink.py
@@ -3,6 +3,6 @@ from time import sleep
 led = Pin(25,Pin.OUT)  # BuitIn led connected to 25 Pin
 while(1):
     led.on()
-    sleep(1)
+    sleep(5)  #Giving 5 Sec delay
     led.off()
-    sleep(1)
+    sleep(5)  #Giving 5 Sec delay
